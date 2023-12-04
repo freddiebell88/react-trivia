c67ab4b96318de3fe1766171fa44fbec5bbaaad0
freddiebell88 <frederikcwbell@gmail.com>
2023-12-04 17:05:57 -0500

    categories loading

--- trivia/src/trivia.jsx
+++ trivia/src/trivia.jsx
@@ -1,23 +1,34 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Quiz } from './quiz'
 
 export function Trivia() {
+    const [categories, setCategories] = useState([])
+    
+    useEffect(() => {
+        axios.get('https://opentdb.com/api_category.php').then((res) => setCategories(res.data.trivia_categories))
+    }, [])
     return(
         <>
         <h1>TRIVIA!</h1>
-        <Categories />
+        <h2>Categories</h2>
+        <div className='categories'>
+            {categories.map((category) => (
+                <Categories 
+                    key={(category.id)}
+                    category={category.name}
+                />
+            ))}
+        </div>
         </>
     )
 }
 
-function Categories() {
-    // const [categories, setCategoires] = useState([])
-
+const Categories = ({ category }) => {
     return(
         <>
-        <h2>Categories</h2>
-        <Quiz />
+            <button>{category}</button>
+        {/* <Quiz /> */}
         </>
     )
 }
