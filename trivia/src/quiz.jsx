8ae38560fa8a4ed4f31c8d88aff1b060bc46f204
freddiebell88 <frederikcwbell@gmail.com>
2023-12-06 19:48:43 -0500

    attempts to show one question at a time and to separate answers

--- trivia/src/quiz.jsx
+++ trivia/src/quiz.jsx
@@ -5,6 +5,8 @@ import {useState, useEffect} from 'react'
 export function Quiz({ selectedCat, categoryId }) {
 
 const [questions, setQuestions] = useState([])
+const [index, setIndex] = useState(0)
+
 useEffect(() => {
     const quizUrl = `https://opentdb.com/api.php?amount=10&category=${categoryId}&type=multiple`
     axios.get(quizUrl).then((res) => {
@@ -17,27 +19,40 @@ useEffect(() => {
         <h2>{selectedCat}</h2>
         <h2>Quiz</h2>
         <div>
-            {questions.map((question) => (
-                <Question 
-                    key={question.question}
-                    question={question.question}
-                    correct_answer={question.correct_answer}
-                    incorrect_answers={question.incorrect_answers}
+            
+                <Question
+                    key={questions.question}
+                    question={questions.question}
+                    correct_answer={questions.correct_answer}
+                    incorrect_answers={questions.incorrect_answers}
                 />
-            ))}
+            
         </div>
         
         </>
     )
 }
 
-function Question( {question, correct_answer, incorrect_answers}) {
+function Question( {question, correct_answer, incorrect_answers, index} ) {
+
+    // const answers = [...incorrect_answers, correct_answer];
+
+    
+    
     return(
-        <div>
+        <>
+        <div className='questionCard'>
             <p>{question}</p>
+            {/* <div>
+                {answers.map((answer) => {
+                    return <button key={question}>{answer}</button>
+                })}
+            </div> */}
             <p>{correct_answer}</p>
             <p>{incorrect_answers}</p>
         </div>
+        <button>Next Question</button>
+        </>
     )
     
 }
